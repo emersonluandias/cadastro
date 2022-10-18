49e4f471819f9d17bc456116d27637776ce20f92
emersonluandias <emersonluandias@gmail.com>
2022-10-17 23:28:48 -0300

    1

---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app', ["current"=> "Produtos"])
+
+@section('body')
+    <h4> Página de Produtos</h4>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\ControladorProduto;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('layout.index');
+});
+
+Route::get('/produtos','App\Http\Controllers\ControladorProduto@index');
+Route::get('/categorias','App\Http\Controllers\ControladorCategoria@index');
+Route::get('/categorias/novo','App\Http\Controllers\ControladorCategoria@create');
+Route::post('/categorias','App\Http\Controllers\ControladorCategoria@store');
+Route::get('/categorias/editar/{id}','App\Http\Controllers\ControladorCategoria@edit');
+Route::get('/categorias/apagar/{id}','App\Http\Controllers\ControladorCategoria@destroy');
